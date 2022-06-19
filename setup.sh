ab038bed65c91b37b570a5a8d1d74362832165ef
TylerS1066 <TylerS1066@users.noreply.github.com>
2022-06-19 01:51:10 +0000

    Create setup script

---
+++ setup.sh
@@ -0,0 +1,31 @@
+# Clone Movecraft
+gh repo clone APDevTeam/Movecraft
+
+# Setup Movecraft
+cd Movecraft
+./setup.sh
+cd ..
+
+# Clone addons
+gh repo clone TylerS1066/Movecraft-Combat
+gh repo clone APDevTeam/Movecraft-Warfare
+gh repo clone APDevTeam/Movecraft-Repair
+gh repo clone APDevTeam/Movecraft-WorldGuard
+gh repo clone TylerS1066/Movecraft-Towny
+gh repo clone TylerS1066/Movecraft-Cannons
+gh repo clone APDevTeam/Beaming
+gh repo clone TylerS1066/Movecraft-CoreProtect
+
+# Clone AP plugins
+gh repo clone APDevTeam/Ap-Turrets
+gh repo clone APDevTeam/APAutoNPC
+gh repo clone APDevTeam/APDonorPerms
+gh repo clone TylerS1066/Deadbolt-Reloaded
+gh repo clone APDevTeam/Staff-Mode
+gh repo clone TylerS1066/Ultimate-FactionsChat
+gh repo clone APDevTeam/WGFactions
+gh repo clone TylerS1066/WGBlockFlags
+gh repo clone APDevTeam/CustomWhiteList
+gh repo clone APDevTeam/iFill
+gh repo clone APDevTeam/APCargoMerchant
+gh repo clone APDevTeam/StructureBoxes
